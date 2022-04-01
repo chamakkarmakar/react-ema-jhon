746b62496bc994aeabd7c927a22ea12cc0cbfc44
chamakkarmakar <chamakkarmakar.ck@gmail.com>
2022-04-01 14:07:43 +0600

    Order & Inventory component created and set their path

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './component/Header/Header';
+import Inventory from './component/Inventory/Inventory';
+import Order from './component/Order/Order';
 import Shop from './component/Shop/Shop';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Shop></Shop>
+      <Routes>
+        <Route path='/' element={<Shop />} />
+        <Route path='/shop' element={<Shop />} />
+        <Route path='/orders' element={<Order />} />
+        <Route path='/inventory' element={<Inventory />} />
+      </Routes>
     </div>
   );
 }
